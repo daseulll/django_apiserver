2126f33ac9d9b2770ddb0bd1fb1522ccb28f7095
daseul <nldaseul@gmail.com>
2019-05-22 17:17:07 +0900

    Implement Post detail/update/Delete view

--- sample_2/urls.py
+++ sample_2/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('post/', views.PostListAPIView.as_view()),
+    path('post/<int:pk>', views.PostDetailAPIView.as_view()),
 ]
--- sample_2/views.py
+++ sample_2/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
@@ -17,3 +18,28 @@ class PostListAPIView(APIView):
             # 직렬화할 data가 serializer.data에 저장
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+# /post/10/ => GET, PUT, DELETE
+class PostDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Post, pk=pk)
+        
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        print(post)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        # serializer의 인자는 (instance, data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serialzer.errors, status=400)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=204)
