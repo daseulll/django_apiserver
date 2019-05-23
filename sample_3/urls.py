b334756b1fd8141da323794b26b0544fe05a9c95
daseul <nldaseul@gmail.com>
2019-05-23 15:18:16 +0900

    Register PostViewSet to URLconf by DefaultRouter

--- sample_3/urls.py
+++ sample_3/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('post', views.PostViewSet)
+# router.urls로 url 목록을 얻을 수 있음
+# 2개의 URL을 처리하는 뷰 함수를 만들어서 등록
+
 urlpatterns = [
-    path('post', views.post_list),
-    path('post/<int:pk>', views.post_detail),
+    path('', include(router.urls)), # /sample_3/post
 ]
--- sample_3/views.py
+++ sample_3/views.py
@@ -5,17 +5,3 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-post_list = PostViewSet.as_view({
-    'get' : 'list',
-    'post' : 'create',
-})
-# post_list 뷰 함수를 생성
-
-post_detail = PostViewSet.as_view({
-    'get' : 'retrieve',
-    'put' : 'update',
-    'patch' : 'partial_update',
-    'delete' : 'destroy',
-})
-
