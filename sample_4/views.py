db5fe8f82e7cd9e32188c17dfaa402d2c21a83d7
daseul <nldaseul@gmail.com>
2019-05-24 15:57:34 +0900

    Post filtering via search_fields

--- sample_4/views.py
+++ sample_4/views.py
@@ -1,13 +1,20 @@
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
 from sample_3.models import Post
 from sample_3.serializers import PostSerializer
-from rest_framework.viewsets import ModelViewSet
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'content']
 
     def get_queryset(self):
         qs = super().get_queryset()
+
+        # search = self.request.GET.get('search', '')
+        # qs = qs.filter(title__icontains=search)
+
         if self.request.user.is_authenticated:
             qs = qs.filter(author=self.request.user)
         else:
