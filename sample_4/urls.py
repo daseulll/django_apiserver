ba3d9c9c79d1b27be017990b9e47841af3b39e31
daseul <nldaseul@gmail.com>
2019-05-24 14:56:42 +0900

    Filtering get_queryset() in APIView

--- sample_4/urls.py
+++ sample_4/urls.py
@@ -1,6 +1,13 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
 
 app_name='sample_4'
 
+router = DefaultRouter()
+router.register('post', views.PostViewSet)
+
+
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- sample_4/views.py
+++ sample_4/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from sample_3.models import Post
+from sample_3.serializers import PostSerializer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author=self.request.user)
+        else:
+            qs = qs.none()  # empty result
+        return qs
+            
