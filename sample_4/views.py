c3deff53ac20f269cb5eb158b3a59910c3c2c4f4
daseul <nldaseul@gmail.com>
2019-05-26 17:42:59 +0900

    Set pagination

--- sample_4/views.py
+++ sample_4/views.py
@@ -2,10 +2,12 @@ from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from sample_3.models import Post
 from sample_3.serializers import PostSerializer
+from .pagination import PostPageNumberPagination
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = PostPageNumberPagination
     filter_backends = [SearchFilter]
     search_fields = ['title', 'content']
 
